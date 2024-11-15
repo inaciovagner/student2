cef1a7831f96604cb401b3066021b3f6d270a165
Vagner de Souza <76208231+inaciovagner@users.noreply.github.com>
2024-11-15 19:44:35 +0000

    Add files via upload
     keyword this

---
+++ Student.java
@@ -0,0 +1,23 @@
+
+/**
+ * Creating a class to model a student
+ *
+ * @author Vagner de Souza
+ * 15/11/24
+ */
+public class Student
+{
+    private int age;
+
+    public Student(int age)
+    {
+        this.age = age;
+    }
+
+    public void whoAmI()
+    {
+        System.out.println(this);
+
+    }
+
+}
